b5e03326c25186e58830f5c50bc86b1fc8fc571f
tchanu <t.chanukvadze@gmail.com>
2018-02-10 13:45:47 +0400

    Node access log

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+let app = require('express')();
+let http = require('http').Server(app);
+let io = require('socket.io')(http);
 let COLORS = ["#F44336", "#EF5350", "#F44336", "#E53935", "#D32F2F", "#C62828", "#B71C1C", "#FF5252", "#FF1744", "#D50000", "#E91E63", "#EC407A", "#E91E63", "#D81B60", "#C2185B", "#AD1457", "#880E4F", "#FF4081", "#F50057", "#C51162", "#9C27B0", "#BA68C8", "#AB47BC", "#9C27B0", "#8E24AA", "#7B1FA2", "#6A1B9A", "#4A148C", "#E040FB", "#D500F9", "#AA00FF", "#673AB7", "#9575CD", "#7E57C2", "#673AB7", "#5E35B1", "#512DA8", "#4527A0", "#311B92", "#7C4DFF", "#651FFF", "#6200EA", "#3F51B5", "#7986CB", "#5C6BC0", "#3F51B5", "#3949AB", "#303F9F", "#283593", "#1A237E", "#536DFE", "#3D5AFE", "#304FFE", "#1E88E5", "#1976D2", "#1565C0", "#0D47A1", "#448AFF", "#2979FF", "#2962FF", "#0288D1", "#0277BD", "#01579B", "#0091EA", "#0097A7", "#00838F", "#006064", "#009688", "#009688", "#00897B", "#00796B", "#00695C", "#004D40", "#43A047", "#388E3C", "#2E7D32", "#1B5E20", "#558B2F", "#33691E", "#827717", "#E65100", "#F4511E", "#E64A19", "#D84315", "#BF360C", "#FF3D00", "#DD2C00", "#795548", "#A1887F", "#8D6E63", "#795548", "#6D4C41", "#5D4037", "#4E342E", "#3E2723", "#757575", "#616161", "#424242", "#212121", "#607D8B"];
 let users = [];
 let typers = [];
+let access = require('fs').createWriteStream(__dirname + '/node.access.log', {flags: 'a'});
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
 
 app.use(require('express').static(__dirname + '/public'));
 
-io.on('connection', function (socket) {
+io.on('connection', (socket) => {
+    access.write((new Date()) + '  connected  ' + socket.handshake.address + '\n');
 
     //auth
-    socket.on('auth', function (name) {
+    socket.on('auth', (name) => {
         socket.color = COLORS[[Math.floor(Math.random() * COLORS.length)]];
         socket.user = name;
         users.push({
@@ -22,6 +24,7 @@ io.on('connection', function (socket) {
             user: socket.user,
             color: socket.color
         });
+        access.write((new Date()) + ' ' + socket.handshake.address + ' |name|  ' + name + '\n');
 
         io.emit('update-online-users', users);
         io.to(socket.id).emit('update-color', socket.color);
@@ -33,7 +36,8 @@ io.on('connection', function (socket) {
     });
 
     //chat handler
-    socket.on('chat', function (msg) {
+    socket.on('chat', (msg) => {
+        access.write((new Date()) + ' ' + socket.handshake.address + '  |msg|  ' + msg + '\n');
         if (typeof socket.user === 'undefined') {
             return;
         }
@@ -46,9 +50,9 @@ io.on('connection', function (socket) {
     });
 
     //color handler
-    socket.on('change-color', function (color) {
+    socket.on('change-color', (color) => {
         socket.color = color;
-        users = users.filter(function (item) {
+        users = users.filter((item) => {
             if (item.id === socket.id) {
                 item.color = socket.color;
             }
@@ -58,13 +62,13 @@ io.on('connection', function (socket) {
     });
 
     //typing handler
-    socket.on('typing', function (msg) {
+    socket.on('typing', (msg) => {
         if (msg) {
             if (typeof socket.user !== 'undefined') {
                 typers.push(socket.user);
             }
         } else {
-            typers = typers.filter(function (item) {
+            typers = typers.filter((item) => {
                 return item !== socket.user
             });
         }
@@ -74,12 +78,15 @@ io.on('connection', function (socket) {
     });
 
     //drawing handler
-    socket.on('drawing', (data) => socket.broadcast.emit('drawing', data));
+    socket.on('drawing', (data) => {
+        socket.broadcast.emit('drawing', data);
+    });
 
-    socket.on('disconnect', function (e) {
-        var left_user = 'Guest';
+    socket.on('disconnect', (e) => {
+        access.write((new Date()) + '  disconnect  ' + socket.handshake.address + '\n');
+        let left_user = 'Guest';
         try {
-            left_user = users.filter(function (item) {
+            left_user = users.filter((item) => {
                 return item.id === socket.id
             })[0].user;
         } catch (err) {
@@ -87,7 +94,7 @@ io.on('connection', function (socket) {
         }
         io.emit('chat', {user: '', data: left_user + ' left the conversation.'});
 
-        users = users.filter(function (item) {
+        users = users.filter((item) => {
             return item.id !== socket.id
         });
         io.emit('update-online-users', users);
@@ -95,6 +102,6 @@ io.on('connection', function (socket) {
 });
 
 
-http.listen(3000, function () {
+http.listen(3000, () => {
     console.log('listening on *:3000');
 });
